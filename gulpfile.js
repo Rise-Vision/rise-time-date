559f4bb03cca64b37d1a2f32772c42e3c4e82cae
Ezequiel Conte <ezequielc@gmail.com>
2021-03-15 12:45:23 -0300

    Add dependencies to bundle.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 const concat = require("gulp-concat");
 const gulp = require("gulp");
 
-const bundle = "dist/rise-time-date.js";
+const bundles = [ 
+	"dist/rise-time-date.js",
+	"dist/rise-time-date-bundle.min.js"
+];
 const dependencies = [
   "node_modules/moment/min/moment.min.js",
   "node_modules/moment-timezone-with-data-2010-2020/index.js"
 ];
 
-gulp.task( "default", () => {
-  return gulp.src( dependencies.concat(bundle) )
-    .pipe( concat( bundle ) )
-    .pipe( gulp.dest( "." ) );
+gulp.task( "default", (done) => {
+  bundles.map(function(file) {
+    return gulp.src( dependencies.concat( file ) )
+      .pipe( concat( file ) )
+      .pipe( gulp.dest( "." ) );
+  });
+  done();
 });
